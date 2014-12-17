e5d61ba13aee04784f3e17cb74a20c02d2e56963
mattshanks <matt@cogent.co>
2014-12-17 11:35:41 +1100

    Updated site description

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<meta name="description" content="The illustration work of Melbourne designer, Matt Shanks">
+	<meta name="description" content="Nursery and Children's illustrations by Melbourne designer, Matt Shanks">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
