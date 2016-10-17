476b548ae7e129d0aba7836bd8deda622b89fdd3
mattshanks <matt@cogent.co>
2016-10-17 11:35:13 +1100

    Update Article page masonry plugin

--- single-article.php
+++ single-article.php
@@ -49,7 +49,7 @@ get_header(); ?>
 								
 					<div class="row related">
 						<h4>Other short adventures</h4>
-							<div id="container">
+							<div id="grid">
 								<?php
 									$args = array( 'posts_per_page' => 5, 'exclude' => $thisPostID, 'category_name' => 'prints');
 									$myposts = get_posts( $args );
