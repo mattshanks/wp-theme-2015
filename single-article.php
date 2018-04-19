126ffef23b9a3ac0a577e3c7769c918252d1cd32
Matt Shanks <matt@cogent.co>
2018-04-19 18:17:15 +1000

    Add newsletter to the bottom of single article pages

--- single-article.php
+++ single-article.php
@@ -29,7 +29,23 @@ get_header(); ?>
 							<div class="story">
 								  <?php echo the_content(); ?>								  
 							</div>
-							
+							<div id="newsletter">
+			<h3>Join the mailing list</h3>
+			<p>Matt sends out an occassional digest of interesting and inspiring art, tools and new techniques. Why not get yours:</p>
+			<!-- Begin MailChimp Signup Form -->
+			 <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+			 <div id="mc_embed_signup">
+			 <form action="//mattshanks.us9.list-manage.com/subscribe/post?u=74a7c46c509135c0f0d710c0f&amp;id=fe6e864968&SIGNUP=Blog" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				 <div id="mc_embed_signup_scroll">
+				 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+				 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_74a7c46c509135c0f0d710c0f_fe6e864968" tabindex="-1" value=""></div>
+				 <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+				 </div>
+			 </form>
+			 </div>
+			 <!--End mc_embed_signup-->
+		</div>
 						</div>
 						
 					</div> <!-- End custom Page -->	
